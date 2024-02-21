221bcd0ad11479728b58f00d32edc0bd507a30c2
Nilai Patel <nilaip96@gmail.com>
2024-02-21 04:12:14 -0800

    Added dealer context not working

--- src/Rooms/GrassyGround/Dealer.js
+++ src/Rooms/GrassyGround/Dealer.js
@@ -1,41 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { useSocket } from "../../SocketContext.js";
+import React from "react";
 
 import Card from "./Card.js";
 import Deck from "./Deck.js";
 import { bestScore } from "../../utils.js";
-
+import { useDealer } from "../../DealerContext.js";
 const Dealer = () => {
-  const socket = useSocket();
-
-  const [dealer, setDealer] = useState({
-    deck: [],
-    hand: [],
-    tossed: [],
-    hidden: false,
-    wallet: 0,
-    position: { x: 50, y: 10 },
-  });
-
-  useEffect(() => {
-    const dealerEvent = (newDealer) => {
-      setDealer(() => newDealer);
-    };
-
-    socket.on("dealer-received", dealerEvent);
-
-    return () => {
-      socket.off("dealer-received", dealerEvent);
-    };
-  }, [dealer, socket]);
-
-  const { hand, deck, tossed, hidden, wallet } = dealer;
+  const { hand, deck, tossed, hidden, wallet } = useDealer();
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
       <div>Dealer</div>
       <div>{wallet}</div>
       <div>
-        {hand.map((card, i) =>
+        {hand?.map((card, i) =>
           hidden && i === 0 ? (
             <Deck deck={["HIDDEN_CARD"]} key={"HIDDEN" + i} />
           ) : (
@@ -43,7 +19,7 @@ const Dealer = () => {
           )
         )}
       </div>
-      <div>{bestScore(hand)}</div>
+      <div>{hand && bestScore(hand)}</div>
       <div>{`Tossed Count: ${tossed.length}`}</div>
       <div>{`Deck Count: ${deck.length}`}</div>
       <Deck deck={deck} />
--- src/Rooms/GrassyGround/Table.js
+++ src/Rooms/GrassyGround/Table.js
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Table.css";
 import Players from "./Players.js";
 import Dealer from "./Dealer.js";
+import { useSocket } from "../../SocketContext.js";
+import { DealerProvider } from "../../DealerContext.js";
 
 const Table = () => {
-  //hand event
-
-  //dealer event
-
   return (
-    <div>
+    <DealerProvider>
       <Dealer />
       <Players />
-    </div>
+    </DealerProvider>
   );
 };
 
